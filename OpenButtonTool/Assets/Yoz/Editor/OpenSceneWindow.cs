152ee2d81346cf7cfb912f58ca71a10283cac8eb
LAPTOP-OCS5O16L\roves <rovesciatablu@gmail.com>
2023-04-12 10:22:36 +0900

    Add new scenes and clean the code.

--- OpenButtonTool/Assets/Yoz/Editor/OpenSceneWindow.cs
+++ OpenButtonTool/Assets/Yoz/Editor/OpenSceneWindow.cs
@@ -10,12 +10,12 @@ namespace Yoz.Editor
 {
     public class OpenSceneWindow : EditorWindow
     {
-        private readonly Vector2 _sceneOpenButtonMinSize = new Vector2(70, 10);
-        private readonly Vector2 _sceneOpenButtonMaxSize = new Vector2(300, 50);
-        private readonly Vector2 _buttonMinSize = new Vector2(100, 20);
-        private readonly Vector2 _buttonMaxSize = new Vector2(1000, 100);
+        private readonly Vector2 _sceneOpenButtonMinSize = new Vector2(100, 10);
+        private readonly Vector2 _sceneOpenButtonMaxSize = new Vector2(400, 50);
+        private readonly Vector2 _setButtonMinSize = new Vector2(100, 20);
+        private readonly Vector2 _setButtonMaxSize = new Vector2(1000, 70);
         private List<string> _sceneNames = new List<string>();
-
+        private bool _flag = false;
 
         [MenuItem("YozTool/SceneWindow")]
         static void ShowWindow()
@@ -25,7 +25,8 @@ namespace Yoz.Editor
 
         private void OnGUI()
         {
-            GUIStyle buttonStyle = new GUIStyle("button") {fontSize = 20};
+            // Set Open Button Layout
+            GUIStyle openButtonStyle = new GUIStyle("button") {fontSize = 20};
             var layoutOptions = new GUILayoutOption[]
             {
                 GUILayout.MinWidth(_sceneOpenButtonMinSize.x),
@@ -33,27 +34,49 @@ namespace Yoz.Editor
                 GUILayout.MaxWidth(_sceneOpenButtonMaxSize.x),
                 GUILayout.MaxHeight(_sceneOpenButtonMaxSize.y),
             };
+            
+            // Set Reload Button Layout
+            GUIStyle setButtonStyle = new GUIStyle("button") {fontSize = 30};
+            setButtonStyle.normal.textColor = Color.cyan;
+            var setLayoutOptions = new GUILayoutOption[]
+            {
+                GUILayout.MinWidth(_setButtonMinSize.x),
+                GUILayout.MinHeight(_setButtonMinSize.y),
+                GUILayout.MaxWidth(_setButtonMaxSize.x),
+                GUILayout.MaxHeight(_setButtonMaxSize.y),
+            };
 
+            // Set Open Button Scenes
             foreach (var sceneName in _sceneNames)
             {
-                Debug.Log(sceneName);
-                if (GUILayout.Button(sceneName, buttonStyle, layoutOptions))
+                // Open Button
+                if (GUILayout.Button(sceneName, openButtonStyle, layoutOptions) || _flag == false)
                 {
+                    _flag = true;
+# if UNITY_EDITOR
                     if (!EditorSceneManager.SaveModifiedScenesIfUserWantsTo(
                             new Scene[] {SceneManager.GetActiveScene()})) return;
-                
+#endif
                     OpenScene(sceneName);
                 }
             }
-            
-            // Refresh Scenes In Build button
-            if (GUILayout.Button("Refresh scenes", buttonStyle, layoutOptions))
+
+            // Reload Button
+            if (GUILayout.Button("Reload Scenes", setButtonStyle, setLayoutOptions))
             {
-                _sceneNames = EditorBuildSettings.scenes
-                    .Where(scene => scene.enabled)
-                    .Select(scene => Path.GetFileNameWithoutExtension(scene.path))
-                    .ToList();
+                ReloadScenes();
             }
+        }
+
+        /// <summary>
+        /// Reload the scenes in Build Settings
+        /// </summary>
+        private void ReloadScenes()
+        {
+            _sceneNames = EditorBuildSettings.scenes
+                .Where(scene => scene.enabled)
+                .Select(scene => Path.GetFileNameWithoutExtension(scene.path))
+                .ToList();
         }
 
         private void OpenScene(string sceneName)
